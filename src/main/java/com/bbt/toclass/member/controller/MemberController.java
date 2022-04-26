be480e577da50e8e1e1d7b38dfa4558cf48d0ece
me <me@DESKTOP-3PL1RS0>
2022-04-26 11:40:11 +0900

    commit34

--- src/main/java/com/bbt/toclass/member/controller/MemberController.java
+++ src/main/java/com/bbt/toclass/member/controller/MemberController.java
@@ -42,5 +42,6 @@ public interface MemberController {
 	ModelAndView findAccountDo(@RequestBody @ModelAttribute MemberVO member) throws Exception;
 	ModelAndView findPwDo(@RequestBody @ModelAttribute MemberVO member) throws Exception;
 	
-	// 찐찐찐찐
+	// interwork test
+	//dkddkdk
 }
